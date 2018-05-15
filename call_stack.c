1becb00f62d955ebb769a55d71edfd80496a1415
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-05-15 21:15:05 +0000

    initial commit - call stacks

---
+++ call_stack.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdli.h>
+
+int fact(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+
+int main(void)
+{
+    printf("%i\n", fact(5));
+
+}
